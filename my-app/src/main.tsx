92e2100d420efa49be430b0b79e77b0ca204185b
enzoestanguera <enzo.estanguera@gmail.com>
2026-09-08 09:45:40 -0300

    feat: aprendendo rotas

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,34 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
-import Home from './routes/Home/index.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import Produtos from './routes/Produtos/index.tsx'
-import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Home from './routes/Home/index.tsx'
 import Error from './routes/Error/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/produtos",
-    element: <Produtos />,
-  },
-  {
-    path: "/editar-produtos",
-    element: <EditarProdutos />,
-  },
-  {
-    path: "*",
-    element: <Error />,
+  {path: '/', element: <App />, errorElement: <Error />, children: [
+      { path: '/', element: <Home /> },
+      { path: '/produtos', element: <Produtos /> },
+      { path: '/editar-produtos', element: <EditarProdutos /> }
+    ]
   },
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
