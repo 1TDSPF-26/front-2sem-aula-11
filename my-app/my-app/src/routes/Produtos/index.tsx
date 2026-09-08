bed5910ebf3c1c9db72502ea0c81f8ecf21958ef
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-08 09:31:31 -0300

    feat: coponentes de rotas finalizado

---
+++ my-app/my-app/src/routes/Produtos/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function index() {
+    document.title = "Produtos"
+  return (
+    <div>index</div>
+  )
+}
---
+++ my-app/my-app/src/routes/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function index() {
+
+    document.title = "Editar Produtos"
+
+  return (
+    <div>index</div>
+  )
+}
