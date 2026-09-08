070b3192fc565b9299ff1ecd3937692faaae1db0
MatheusRodriguesSerrao <rmatheusrodrigues193@gmail.com>
2026-09-08 09:28:46 -0300

    feat: Criando componetes de rotas.

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,9 @@
+export default function EditarProdutos() {
+   document.title = "Editar Produtos";
+
+  return (
+    <main>
+        <h2>Editar Produtos</h2>
+    </main>
+  );
+}
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+  document.title = "Erro 404";
+
+  return (
+    <main>
+      <h2>404 - Página não encontrada</h2>
+    </main>
+  );
+}
