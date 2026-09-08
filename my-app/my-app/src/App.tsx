6f6ce38f0189d5c1bea2ac411b0cba99949d9592
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-08 09:54:00 -0300

    feat: refactor App component to use RouterProvider with routes

--- my-app/my-app/src/App.tsx
+++ my-app/my-app/src/App.tsx
@@ -1,13 +1,3 @@
-import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Conteudo from "./components/Conteudo/Conteudo";
-import Rodape from "./components/Rodape/Rodape";
-
-export default function App() {
-  return (
-    <div>
-      <Cabecalho/>
-      <Conteudo/>
-      <Rodape/>
-    </div>
-  )
-}
+import { Outlet } from "react-router-dom";
+import Lampada from "./components/lampada";
+import 
