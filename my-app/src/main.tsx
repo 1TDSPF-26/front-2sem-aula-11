3030223d3c7a1aed4e607ad857b12ec1922c879c
giovvane <giovanedc007@gmail.com>
2026-09-08 09:43:09 -0300

    feat: Vetor de rotas criado.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,13 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider} from 'react-router'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produto/index.tsx'
 import EditarProdutos from './routes/EditarProdutos/index.tsx'
 import Error from './routes/Error/index.tsx'
-import Home from './routes/Home/index.tsx'
-import Produto from './routes/Produto/index.tsx'
+
+const router = createBrowserRouter([
+  {path:"/", element: <App/>, errorElement:<Error/>, children:[
+    {path: "/", element: <Home/>,}
+    {path: "/produtos", element:<Produtos/>}
+    {path: "/editar-produtos/:id", element:<EditarProdutos/>}
+  ]}
+]);
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
